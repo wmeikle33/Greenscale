097f9c118cdef9c88905f692bcb13f66034012f4
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-06-03 12:51:44 +0800

    Create run_workflow.py

---
+++ scripts/run_workflow.py
@@ -0,0 +1,9 @@
+from image_analysis.pipeline import main as run_pipeline
+from image_analysis.train import main as run_train
+
+def main() -> None:
+    run_pipeline()
+    run_train()
+
+if __name__ == "__main__":
+    main()
